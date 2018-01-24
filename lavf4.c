fdb91d4db3acce03713305e6ef96a045bceeda59
lauravaldes <35716815+lauravaldes@users.noreply.github.com>
2018-01-24 12:33:00 -0600

    Add files via upload

---
+++ lavf4.c
@@ -0,0 +1,19 @@
+//  Valdes Flores Laura Alicia
+//  Tarea4
+//
+//  Created by Laura Flores on 8/22/17.
+//  Copyright © 2017 Laura Flores. All rights reserved.
+//
+
+#include <stdio.h>
+
+
+int main()
+{
+    char letter;
+    printf("Escribe una letra: ");
+    scanf("%c",&letter);
+    printf("El caracter %c corresponde al numero %d en ASCII\n\n\n", letter, letter);
+    
+}
+
